8ba71ba3d473b7f98b3047051af0fd4cad6d3eb5
WayanBerdyanto <aber.panak.atok@gmail.com>
2023-12-01 09:45:20 +0700

    Update Shadow in ListItem

--- app/src/main/java/com/example/ecoscan/ui/component/ListItem.kt
+++ app/src/main/java/com/example/ecoscan/ui/component/ListItem.kt
@@ -1,23 +1,20 @@
 package com.example.ecoscan.ui.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -35,10 +32,14 @@ fun ListItems(
     year: String,
     modifier: Modifier = Modifier
 ) {
-    Card(
+    androidx.compose.material.Card(
         shape = MaterialTheme.shapes.medium,
         modifier = Modifier
             .padding(vertical = 8.dp, horizontal = 12.dp)
+            .shadow(
+                10.dp,
+                RoundedCornerShape(8.dp)
+            )
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
