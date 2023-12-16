2cbc8e81ce2b2f474db1705e048c23c45fb206b3
M ARIF SATRIA <arifsatria5554@gmail.com>
2023-12-16 23:31:48 +0700

    Feat: Scan Still Not Working

--- app/src/main/java/com/example/ecoscan/di/Injection.kt
+++ app/src/main/java/com/example/ecoscan/di/Injection.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.example.ecoscan.data.pref.UserPreference
 import com.example.ecoscan.data.pref.dataStore
 import com.example.ecoscan.data.remote.retrofit.ApiConfig
+import com.example.ecoscan.data.remote.retrofit.PredictApiConfig
 import com.example.ecoscan.data.repository.EcoRepository
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
@@ -15,4 +16,14 @@ object Injection {
         val apiService = ApiConfig.getApiService(user.token)
         return EcoRepository.getInstance(apiService, pref)
     }
+}
+
+object PredictInjection {
+    fun provideRepository(context: Context): EcoRepository {
+        val predictApiService = PredictApiConfig.getApiService()
+        val pref = UserPreference.getInstance(context.dataStore)
+        val user = runBlocking { pref.getSession().first() }
+        return EcoRepository.getInstance(predictApiService,pref)
+    }
+
 }
